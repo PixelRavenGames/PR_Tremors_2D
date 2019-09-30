5073ef38738add3da1665e9eece3e79a6c6039c6
CrazM13 <sepulvedajoey13@yahoo.com>
2019-09-30 16:43:12 -0400

    Added cloud movement

---
+++ Assets/_Scripts/Environment/CloudMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+	public bool randomizeSpeed = false;
+	[ConditionalHide("randomizeSpeed", true, true)]
+	public float speed = 1f;
+	[ConditionalHide("randomizeSpeed", true, false)]
+	public float minSpeed = 1f;
+	[ConditionalHide("randomizeSpeed", true, false)]
+	public float maxSpeed = 1f;
+
+	private Vector3 startPos;
+	private Vector3 endPos;
+
+	void Start() {
+		startPos = transform.position;
+		endPos = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+	}
+
+	void Update() {
+
+	}
+
+	private void OnDrawGizmos() {
+		Gizmos.DrawLine(transform.position, new Vector3(-transform.position.x, transform.position.y, transform.position.z));
+	}
+}
