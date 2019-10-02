2f9348b270c2179bd6949db32f47ab3ccc4a4e50
Kyle <arow411@gmail.com>
2019-10-02 18:35:01 -0400

    Added some sounds and particle effects and restructured the camera

---
+++ Assets/_Scripts/Environment/ParticleSplatter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.XR.WSA;
+
+public class ParticleSplatter : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particles;
+
+    public void Splatter(Vector3 position, float weight)
+    {
+        var splatter = Instantiate(particles);
+        splatter.transform.position = position;
+
+        
+        splatter.emission.SetBursts(new ParticleSystem.Burst[]
+        {
+            new ParticleSystem.Burst(0.0f, (short) (30 * weight), (short) (40 * weight))
+        });
+        
+        /*var emission = splatter.emission;
+        
+        emission.rateOverTimeMultiplier *= weight;
+        
+        var bursts = new ParticleSystem.Burst[emission.burstCount];
+        emission.GetBursts(bursts);
+
+        foreach (var burst in bursts)
+        {
+            var curve = burst.count;
+            curve.curveMultiplier *= weight;
+        }
+        
+        splatter.emission.SetBursts(bursts);*/
+    }
+}
