370a0e7b171b81f9de937dd68cf73adaba4153ee
Kyle <Arow411@gmail.com>
2019-10-06 18:48:04 -0400

    Make meteor spawn zone configurable

--- Assets/_Scripts/MeteorManager.cs
+++ Assets/_Scripts/MeteorManager.cs
@@ -11,6 +11,7 @@ public class MeteorManager : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private AnimationCurve meteorRate;
     [SerializeField] private float maxMeteorTime;
+    [SerializeField] private BoxCollider2D spawnArea;
     private float timePassed = 0f;
     
     void Update()
@@ -23,7 +24,12 @@ public class MeteorManager : MonoBehaviour
         if (Random.Range(0f, 1f) < Time.deltaTime * meteorRate.Evaluate(timePassed / maxMeteorTime))
         {
             var meteor = Instantiate(meteorPrefab);
-            meteor.transform.position = new Vector3(Random.Range(-2f, 2f), 3, 0);
+            var colliderPos = (Vector2) spawnArea.transform.position + spawnArea.offset;
+            meteor.transform.position = new Vector3(
+                Random.Range(colliderPos.x - spawnArea.size.x / 2, colliderPos.x + spawnArea.size.x / 2),
+                Random.Range(colliderPos.y - spawnArea.size.y / 2, colliderPos.y + spawnArea.size.y / 2),
+                0
+            );
             meteor.meteorVelocity = new Vector2(Random.Range(-1.5f, 1.5f), Random.Range(-5.5f, -6.5f));
         }
     }
