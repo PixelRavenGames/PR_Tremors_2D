d65d217d9a468df95d6e3e731d1b645f6d20f972
CrazM13 <sepulvedajoey13@yahoo.com>
2019-10-08 16:38:33 -0400

    Added Lobby Menu. Added character picker (not yet connected to main game)

---
+++ Assets/_Scripts/Human/HumanSpriteSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Human Sprites", menuName = "Human/Sprites", order = 1)]
+public class HumanSpriteSelection : ScriptableObject {
+
+	[SerializeField]
+	private Sprite[] male;
+	[SerializeField]
+	private Sprite[] female;
+
+	public Sprite GetSprite(bool maleFlg, int index) {
+		return maleFlg ? male[index] : female[index];
+	}
+
+	public int GetSpriteCount(bool maleFlg) {
+		return maleFlg ? male.Length : female.Length;
+	}
+
+}
---
+++ Assets/_Scripts/UI/CharacterSelection.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour {
+
+	public int controllerNumber;
+	public Image preview;
+	public HumanSpriteSelection sprites;
+
+	private int index;
+	private bool male;
+
+	void Start() {
+		index = 0;
+		male = true;
+
+		UpdateChange();
+	}
+
+	public void Next() {
+		index++;
+		index %= sprites.GetSpriteCount(male);
+
+		UpdateChange();
+	}
+
+	public void Prev() {
+		index--;
+		if (index < 0) index += sprites.GetSpriteCount(male);
+
+		UpdateChange();
+	}
+
+	public void Randomize() {
+		index = Random.Range(0, sprites.GetSpriteCount(male));
+
+		UpdateChange();
+	}
+
+	public void SetMale() {
+		male = true;
+		index %= sprites.GetSpriteCount(male);
+
+		UpdateChange();
+	}
+
+	public void SetFemale() {
+		male = false;
+		index %= sprites.GetSpriteCount(male);
+
+		UpdateChange();
+	}
+
+	private void UpdateChange() {
+		preview.sprite = sprites.GetSprite(male, index);
+	}
+
+}
